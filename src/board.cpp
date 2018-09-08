0589e80dad2221262b0357fe8a296f98acf8ef78
Chetan Mittal <mittalchetan98@gmail.com>
2018-09-08 15:41:34 +0530

    move functions

--- src/board.cpp
+++ src/board.cpp
@@ -12,7 +12,7 @@ Board::Board(int board_size=5, int given_rings=5, int rings_to_remove=3, int mar
 	white_rings_out=0;
 
 	// initialise board to all zeroes
-	board = new vector<vector<int>>((board_size+1));
+	board = vector<vector<int>>((board_size+1));
 	board[0].push_back(0);
 	for (int i=1;i<=board_size;i++)
 	{
@@ -20,3 +20,21 @@ Board::Board(int board_size=5, int given_rings=5, int rings_to_remove=3, int mar
 			board[i].push_back(0);
 	}
 }
+
+bool Board::placeRing(int ring_no, location l){
+	if(l.hexagon > board_size || l.position >= 6*hexagon)
+		return false;
+	board[l.hexagon][l.position] = ring_no;
+	return true;
+}
+
+bool Board::moveRing(int ring_no, location start, location end){
+	board[start.hexagon][start.position] = ring_no/2;
+	board[end.hexagon][end.position] = ring_no;
+	int h1 = start.hexagon;
+	int p1 = start.position;
+	int h2 = end.hexagon;
+	int p2 = end.position;
+}
+bool removeMarkerSeq(location start, location end);
+bool removeRing(location l);
--- src/board.h
+++ src/board.h
@@ -5,15 +5,23 @@
 #include <cstdlib>
 using namespace std;
 
+struct location
+{
+	int hexagon;
+	int position;
+};
+
 class Board{
-	public:
-		vector<vector<int>> board;
+	private:
+		vector<vector<int>> board;		// 0 -> empty, 1 -> black marker, 2 -> black ring, -1 -> white marker, -2 -> white ring
 		int board_size;			// N
 		int given_rings;		// M
 		int rings_to_remove;	// L
 		int markers_in_line;	// K
 		int black_markers;
 		int white_markers;
+		int black_rings_in;
+		int white_rings_in;
 		int black_rings_out;
 		int white_rings_out;
 		int turn_id;
@@ -21,13 +29,20 @@ class Board{
 		vector<pair<int,int>> white_rings;
 		vector<pair<int,int>> black_rings;
 
-		Board(int board_size, int given_rings, int rings_to_remove, int markers_in_line);
+		// ring_no : black ring << 2, white ring << -2
+		// These function do not check the validity of the move, they can only tell if the location is outside the board
+		bool placeRing(int ring_no, location l);
+		bool moveRing(location start, location end);
+		bool removeMarkerSeq(location start, location end);
+		bool removeRing(location l);
 
+	public:
+		Board(int board_size, int given_rings, int rings_to_remove, int markers_in_line);
 		Board* execute_move();
 
 		int eval_func();
 
 		vector<Board*> get_valid_moves(int player_id);
-}
+};
 
 #endif
