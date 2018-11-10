387037712c89d01efe9a58715b19f5ab00ef906c
deepanshu jindal <deepanshujindal.99@gmail.com>
2018-11-10 21:01:47 +0530

    place ring made deterministic, experiment with edge rings

--- src/bot.cpp
+++ src/bot.cpp
@@ -80,6 +80,60 @@ location Bot::best_place_ring(){
 	double randomising_seed;
 	Board* newBoard = board->copy_board();
 
+	// randomised policy
+	// for (int i=1;i<board_size;i++){
+	// 	l.hexagon=i;
+	// 	for (int j=0;j<6*i;j++){
+	// 		l.position=j;
+	// 		c = location_to_coordinates(l);
+	// 		if (board->board[c.x][c.y]!=0)
+	// 			continue;
+	// 		randomising_seed=double(rand())/RAND_MAX;
+	// 		newBoard->board[c.x][c.y]=2*player_id;
+	// 		block = blocked_rings(newBoard->board, c) + 2*self_blocked_rings(newBoard->board, c);
+	// 		if ((block>max_block && randomising_seed>0.5) || (block==max_block && randomising_seed>(0.8 + 0.02*i))){
+	// 			max_block=block;
+	// 			best_coord.x=c.x;
+	// 			best_coord.y=c.y;
+	// 		}
+	// 		newBoard->board[c.x][c.y]=0;
+	// 	}
+	// }
+
+	// l.hexagon=board_size;
+	// for (int j=0;j<6*board_size;j++){
+	// 	l.position=j;
+	// 	c = location_to_coordinates(l);
+	// 	if (board->board[c.x][c.y]!=0)
+	// 		continue;
+	// 	randomising_seed=double(rand())/RAND_MAX;
+	// 	newBoard->board[c.x][c.y]=2*player_id;
+	// 	block = blocked_rings(newBoard->board, c) + 2*self_blocked_rings(newBoard->board, c);
+	// 	if (block>=max_block && randomising_seed>0.5){
+	// 		max_block=block;
+	// 		best_coord.x=c.x;
+	// 		best_coord.y=c.y;
+	// 	}
+	// 	newBoard->board[c.x][c.y]=0;
+	// }
+
+	// deterministic for deterministic bots
+	l.hexagon=board_size;
+	for (int j=0;j<6*board_size;j++){
+		l.position=j;
+		c = location_to_coordinates(l);
+		if (board->board[c.x][c.y]!=0)
+			continue;
+		newBoard->board[c.x][c.y]=2*player_id;
+		block = blocked_rings(newBoard->board, c) + 2*self_blocked_rings(newBoard->board, c);
+		if (block>=max_block){
+			max_block=block;
+			best_coord.x=c.x;
+			best_coord.y=c.y;
+		}
+		newBoard->board[c.x][c.y]=0;
+	}
+
 	for (int i=1;i<board_size;i++){
 		l.hexagon=i;
 		for (int j=0;j<6*i;j++){
@@ -87,10 +141,9 @@ location Bot::best_place_ring(){
 			c = location_to_coordinates(l);
 			if (board->board[c.x][c.y]!=0)
 				continue;
-			randomising_seed=double(rand())/RAND_MAX;
 			newBoard->board[c.x][c.y]=2*player_id;
 			block = blocked_rings(newBoard->board, c) + 2*self_blocked_rings(newBoard->board, c);
-			if ((block>max_block && randomising_seed>0.5) || (block==max_block && randomising_seed>(0.8 + 0.02*i))){
+			if (block>=max_block){
 				max_block=block;
 				best_coord.x=c.x;
 				best_coord.y=c.y;
@@ -99,22 +152,6 @@ location Bot::best_place_ring(){
 		}
 	}
 
-	l.hexagon=board_size;
-	for (int j=0;j<6*board_size;j++){
-		l.position=j;
-		c = location_to_coordinates(l);
-		if (board->board[c.x][c.y]!=0)
-			continue;
-		randomising_seed=double(rand())/RAND_MAX;
-		newBoard->board[c.x][c.y]=2*player_id;
-		block = blocked_rings(newBoard->board, c) + 2*self_blocked_rings(newBoard->board, c);
-		if (block>=max_block && randomising_seed>0.5){
-			max_block=block;
-			best_coord.x=c.x;
-			best_coord.y=c.y;
-		}
-		newBoard->board[c.x][c.y]=0;
-	}
 	delete newBoard;
 	best_location = coordinates_to_location(best_coord);
 	return best_location;
@@ -134,20 +171,54 @@ void Bot::place_ring(){
 	int rings_placed=0;
 	location l;
 	coordinates c;
+	int pot_positions[7] = {0,7,8,9,11,10,12};
+	bool use_pot_positions=false;
+	if (board_size==6 && Board::markers_in_line==5){
+		use_pot_positions=false;		
+	}
+
 	while(rings_placed<Board::given_rings){
 		double time_tick = clock();
 		rings_placed++;
 		
-		if (board->board[board_size][board_size]==0){
-			move="P 0 0";
-			board->execute_move(move);
-			cout<<move<<endl;
-		}
-		else{
-			l = best_place_ring();
-			move="P "+to_string(l.hexagon)+" "+to_string(l.position);
-			board->execute_move(move);
-			cout<<move<<endl;
+		// if (use_pot_positions){			
+		// 	for (int i=7;i<12;i++){
+		// 		l.hexagon = 6;
+		// 		l.position = pot_positions[rings_placed];
+		// 		c = location_to_coordinates(l);
+		// 		if (board->board[c.x][c.y]==(-2*player_id) || board->board[c.x][c.y]==(-1*player_id)){
+		// 			use_pot_positions=false;
+		// 		}
+		// 		cerr<<i<<" "<<board->board[c.x][c.y]<<endl;
+		// 	}
+			
+		// 	l.hexagon = 6;
+		// 	l.position = pot_positions[rings_placed];
+		// 	c = location_to_coordinates(l);
+			
+		// 	if (checkValid(c) && board->board[c.x][c.y]==0 && use_pot_positions){
+				
+		// 		move = "P " + to_string(6) + " " + to_string(pot_positions[rings_placed]);
+		// 		board->execute_move(move);
+		// 		cout<<move<<endl;
+		// 	}
+		// 	else{
+		// 		use_pot_positions=false;
+		// 	}
+			
+		// }
+		if (!use_pot_positions){
+			if (board->board[board_size][board_size]==0){
+				move="P 0 0";
+				board->execute_move(move);
+				cout<<move<<endl;
+			}
+			else{
+				l = best_place_ring();
+				move="P "+to_string(l.hexagon)+" "+to_string(l.position);
+				board->execute_move(move);
+				cout<<move<<endl;
+			}
 		}
 		time_left -= ((double)(clock()-time_tick))/CLOCKS_PER_SEC;
 		getline(cin,move);
@@ -220,7 +291,8 @@ void Bot::minimax_decision(int MOVE_NUMBER){
 void Bot::play(){
 	place_ring();
 	int MAX_MOVES=100;
-
+	if (board_size>5)
+		DEEP_MAX_DEPTH = 2;
 	while(MAX_MOVES>0){
 		MAX_MOVES--;
 		double time_tick=clock();
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char*argv[])
 {
 	int player_id, time_limit, seq_length=5, given_rings;
 	string dummy;
-	cin >> player_id >> board_size >> time_limit; //>> seq_length;
+	cin >> player_id >> board_size >> time_limit >> seq_length;
 	getline(cin,dummy);
 	given_rings = board_size;
 	Board::set_board_params(board_size,given_rings,3,seq_length);
